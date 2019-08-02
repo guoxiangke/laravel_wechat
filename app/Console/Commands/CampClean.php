57e3ba1613f6478edc5826c3d155995ab7e65860
guoxiangke <xiangkeguo@gmail.com>
2019-08-02 19:14:57 +0800

    php artisan camp:clean

---
+++ app/Console/Commands/CampClean.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Carbon\Carbon;
+use App\Models\Gamp;
+use Illuminate\Console\Command;
+
+class CampClean extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'gamp:clean';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Delete 2 monthes ago gamps';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $start = Carbon::now()->subMonths(2)->startOfMonth();
+        Gamp::where('created_at', '<', $start)->forceDelete();
+    }
+}
